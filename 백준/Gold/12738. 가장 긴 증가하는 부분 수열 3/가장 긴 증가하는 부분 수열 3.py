662908d803b0fef45b96eeb4d97bc4d20b5bfe57
정재엽 <49589140+woduq1414@users.noreply.github.com>
2022-08-18 23:28:02 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 668 ms, Memory: 265204 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
@@ -0,0 +1,24 @@
+import sys
+
+import bisect
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+c = [10 ** 9] * (n + 1)
+c[0] = -10 ** 9
+c[1] = arr[0]
+longest_len = 1
+
+for i, num in enumerate(arr[1:]):
+
+    if c[longest_len] < num:
+        c[longest_len + 1] = num
+        longest_len += 1
+    else:
+        idx = bisect.bisect_left(c, num)
+        c[idx] = num
+
+print(longest_len)
