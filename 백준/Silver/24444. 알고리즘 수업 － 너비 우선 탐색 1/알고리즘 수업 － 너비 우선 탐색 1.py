8fbc8ce6d6b670d8ffe4db1f03b969a3b7fbf594
정재엽 <49589140+woduq1414@users.noreply.github.com>
2022-08-20 23:31:26 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 628 ms, Memory: 60980 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,51 @@
+import sys
+import heapq
+from collections import deque, defaultdict
+
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+
+graph = [[] for i in range(n + 1)]
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i, g in enumerate(graph):
+    graph[i].sort()
+
+result = [0] * (n + 1)
+c = 1
+
+
+def bfs(graph, s):
+    global result, c
+
+    visited = [False for i in range(n + 1)]
+    queue = deque()
+
+    queue.append(s)
+    visited[s] = True
+    result[s] = c
+    c += 1
+
+    while queue:
+        node = queue.popleft()
+
+        for near_node in graph[node]:
+            if visited[near_node] is False:
+
+                result[near_node] = c
+                c+=1
+
+
+                visited[near_node] = True
+                queue.append(near_node)
+
+    return visited
+
+bfs(graph, r)
+
+for node in result[1:]:
+    print(node)
