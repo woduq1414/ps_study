574c8fa2afcf174a5087b40c288354a333751c5b
정재엽 <49589140+woduq1414@users.noreply.github.com>
2022-09-06 12:28:09 +0900

    [Silver III] Title: 1로 만들기, Time: 96 ms, Memory: 38744 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n = int(input())
+
+
+def bfs(s):
+    visited = [0] * 1000001
+    queue = deque()
+
+    queue.append([s])
+
+
+
+    while queue:
+
+        node = queue.popleft()
+        node_last = node[-1]
+        if node_last == 1:
+            return visited[1], node
+
+
+        a, b = divmod(node_last, 2)
+        c, d = divmod(node_last, 3)
+
+        near_node_list = [node_last - 1]
+        if b == 0 :
+            near_node_list.append(a)
+        if d == 0 :
+            near_node_list.append(c)
+
+
+        for near_node in near_node_list:
+
+            if visited[near_node] == 0:
+                visited[near_node] = visited[node_last] + 1
+                queue.append(node + [near_node])
+
+
+c, lis = bfs(n)
+print(c)
