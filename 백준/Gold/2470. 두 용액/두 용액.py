4a8450437ccc4846c1a5f6e6e087ece41ba3c6a2
정재엽 <49589140+woduq1414@users.noreply.github.com>
2022-09-07 16:29:50 +0900

    [Gold V] Title: 두 용액, Time: 264 ms, Memory: 63484 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,30 @@
+import sys
+import math
+
+input = sys.stdin.readline
+
+n = int(input())
+
+arr = list(map(int, input().split()))
+arr.sort()
+
+abs_arr = [ abs(x) for x in arr]
+
+
+result = []
+
+left = 0
+right = n - 1
+
+while left != right:
+
+    result.append([abs(arr[left] + arr[right]), [left, right]])
+
+    if abs_arr[left] >= abs_arr[right]:
+        left += 1
+    else:
+        right -= 1
+
+result.sort(key = lambda x : x[0])
+
+print(arr[result[0][1][0]], arr[result[0][1][1]], sep=" ")
