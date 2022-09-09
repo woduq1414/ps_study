bd7773396ac7c77f08aaffb544b816ec47c308ec
정재엽 <49589140+woduq1414@users.noreply.github.com>
2022-09-10 00:36:37 +0900

    [Gold III] Title: 큐브러버, Time: 780 ms, Memory: 34912 KB -BaekjoonHub

---
+++ 백준/Gold/3981. 큐브러버/큐브러버.py
@@ -0,0 +1,33 @@
+import sys
+from decimal import *
+
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    data = list(map(int, input().split()))
+    n, arr = data[0], data[1:]
+
+    if len(arr) <= 4:
+        print("YES")
+    else:
+        x, y, z, w = arr[0:4]
+
+        a = Decimal((w - x + 3 * y - 3 * z) / 6)
+        b = Decimal((-2 * w + 3 * x - 8 * y + 7 * z) / 2)
+        c = Decimal((11 * w - 26 * x + 57 * y - 42 * z) / 6)
+        d = Decimal(-w + 4 * x - 6 * y + 4 * z)
+
+        f = True
+        for i in range(4, len(arr)):
+            j = i + 1
+            answer = Decimal(
+                a * Decimal(j ** 3) + b * Decimal(j ** 2) + c * Decimal(j) + d)
+            if abs(Decimal(arr[i]) - answer) >= 0.00001:
+                f = False
+
+        if f is False:
+            print("NO")
+        else:
+            print("YES")
