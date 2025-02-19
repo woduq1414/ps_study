8e61932a38ac470a4c5983de5605733495785d61
정재엽 <49589140+woduq1414@users.noreply.github.com>
2025-02-20 02:52:07 +0900

    [Gold V] Title: 평범한 배낭, Time: 176 ms, Memory: 111508 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,32 +1,18 @@
 import sys
 
-input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 8)
+
+def input(): return sys.stdin.readline().rstrip()
+
 
 n, k = map(int, input().split())
-arr = [list(map(int, input().split())) for i in range(n)]
 
-memo = [[0] * (k + 1) for i in range(n)]
+dp = [0] * (k + 1)
 
-if arr[0][0] <= k:
-    memo[0][arr[0][0]] = arr[0][1]
+for i in range(n):
+    w, v = map(int, input().split())
+    for j in reversed(range(0, k - w + 1, 1)):
 
-for i in range(1, n):
-    w, v = arr[i]
+        dp[j + w] = max(dp[j + w], dp[j] + v)
 
-    if w > k:
-        for j in range(1, k + 1):
-            memo[i][j] = memo[i - 1][j]
-    else:
-
-        for j in range(1, k + 1):
-
-            memo[i][j] = memo[i - 1][j]
-
-            if j == w:
-                memo[i][j] = max(memo[i][j], v)
-            elif j > w:
-                memo[i][j] = max(memo[i][j], memo[i - 1][j - w] + v)
-
-
-print(max(max(memo[-1]), 0))
+# print(dp)
+print(max(dp))
