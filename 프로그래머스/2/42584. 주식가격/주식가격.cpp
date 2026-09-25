127c7e46c2c4a64d6e160aea6dcfd6c21407a654
정재엽 <49589140+woduq1414@users.noreply.github.com>
2026-09-25 05:44:08 -0700

    [level 2] Title: 주식가격, Time: 23.97 ms, Memory: 27.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer(prices.size());
+    fill(answer.begin(), answer.end(), 0);
+    
+    vector<pair<int, int>> stack;
+    
+    for(int i = 0 ; i < prices.size() ; i++){
+        
+        for(int j = stack.size() - 1 ; j >= 0 ; j --){
+            auto& item = stack[j];
+            if(prices[i] < item.first){
+                answer[item.second] = i - item.second;
+                
+                stack.pop_back();
+            }
+        }
+        
+        stack.push_back({prices[i], i});
+        
+    }
+    
+    for(auto& item : stack){
+        answer[item.second] = prices.size() - item.second - 1;
+    }
+    
+    return answer;
+}
