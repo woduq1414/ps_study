b4fe2b3ef428af36e6d9c3c27d5db7b4b24aec24
정재엽 <49589140+woduq1414@users.noreply.github.com>
2026-09-25 19:30:48 -0700

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.02 ms, Memory: 4.54 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,51 +1,60 @@
-#include <string>
 #include <vector>
-#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <cstdlib>
 
 using namespace std;
 
-int dfs(int node, const vector<vector<int>>& arr, vector<bool>& visited, int cnt){
-    visited[node] = true;
-    
-    // cout << node << endl;
-    for(int i = 1 ; i <= arr.size() ; i ++){
-        if(!visited[i] && arr[node][i] == 1){
-            cnt = dfs(i, arr, visited, cnt + 1);
-        }
+int answer;
+
+int dfs(
+    int node,
+    int parent,
+    int n,
+    const vector<vector<int>>& graph
+) {
+    int subtreeSize = 1;
+
+    for(int next : graph[node]) {
+
+        // 부모로 다시 돌아가지 않음
+        if(next == parent)
+            continue;
+
+        int childSize = dfs(
+            next,
+            node,
+            n,
+            graph
+        );
+
+        // node - next 간선을 끊는 경우
+        int diff = abs(
+            childSize - (n - childSize)
+        );
+
+        answer = min(answer, diff);
+
+        subtreeSize += childSize;
     }
-    
-    return cnt;
-    
+
+    return subtreeSize;
 }
 
-
 int solution(int n, vector<vector<int>> wires) {
-    int m = -1;
-    vector<vector<int>> arr(n + 1, vector<int>(n + 1, 0));
-    for(int i = 0; i < n - 1 ; i ++){
-        vector<int> wire = wires[i];
-        arr[wire[0]][wire[1]] = 1;
-        arr[wire[1]][wire[0]] = 1;
+    vector<vector<int>> graph(n + 1);
+
+    for(auto& wire : wires) {
+        int a = wire[0];
+        int b = wire[1];
+
+        graph[a].push_back(b);
+        graph[b].push_back(a);
     }
-    for(int i = 0 ; i < n - 1 ; i ++){
-        vector<int> wire = wires[i];
-        arr[wire[0]][wire[1]] = 0;
-        arr[wire[1]][wire[0]] = 0;
-        
-        vector<bool> visited(n + 1, false);
-        int cnt = dfs(wire[0], arr, visited, 1);
-        cnt = min(cnt, n - cnt);
-     
-        if(cnt > m){
-            m = cnt;
-        }
-        
-        arr[wire[0]][wire[1]] = 1;
-        arr[wire[1]][wire[0]] = 1;
-    }
-    
-    
-    
-    int answer = n - 2 * m;
+
+    answer = INT_MAX;
+
+    dfs(1, -1, n, graph);
+
     return answer;
 }
